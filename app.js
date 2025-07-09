fa96f0256798ceb37b091e62239cb1ec8161e331
maxdibbe <128843656+maxdibbe@users.noreply.github.com>
2025-07-09 11:38:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-// Firebase config (sostituisci con i tuoi valori)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCnXR4StZ2ri3AdB3gNgfWxNaN9nYyBIOQ",
   authDomain: "dfilamentapp.firebaseapp.com",
@@ -7,6 +12,9 @@ const firebaseConfig = {
   messagingSenderId: "592511326712",
   appId: "1:592511326712:web:55c49e3a13d918a0fc9e0d"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
