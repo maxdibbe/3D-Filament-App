1664bcdfd88a1794778452e87d47f0ea5782aa92
maxdibbe <128843656+maxdibbe@users.noreply.github.com>
2025-07-09 11:54:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 const firebaseConfig = {
   apiKey: "AIzaSyCnXR4StZ2ri3AdB3gNgfWxNaN9nYyBIOQ",
   authDomain: "dfilamentapp.firebaseapp.com",
@@ -16,6 +13,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+const db = getFirestore(app);
 
 function login() {
   const email = document.getElementById("email").value;
